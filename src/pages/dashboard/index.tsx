4da108dee8330a582bf3f41aeb16fa84a72633df
Nalberth <nalberthsantosr@gmail.com>
2022-11-22 15:10:42 -0300

    fix: check if user is authenticated

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Header } from '../../components'
+// Dependencies
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Context
+import { useAuth } from '../../context/AuthenticationContext';
+
+// Components
+import { Aside, Header } from '../../components';
+
+// Styles
+import './styles.scss';
 
 export const Dashboard = () => {
+  const { isAuthenticated } = useAuth();
+  const history = useHistory();
+
+  if (!isAuthenticated) history.push('/');
+
   return (
     <>
       <Header />
-      
+
+      <div className="main__content">
+        <Aside />
+        <main>
+          <h1>Content</h1>
+        </main>
+      </div>
     </>
-  )
-}
+  );
+};
