2cff3c835f29928f5e92d59f488038632df0d3f8
Nalberth <nalberthsantosr@gmail.com>
2022-11-22 16:06:58 -0300

    feat: add RevenueTime

---
+++ src/pages/dashboard/components/CompanyDashboard/RevenueTime/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// Styles
+import './styles.scss'
+
+export const RevenueTime = () => {
+  return (
+    <div className="revenueTime">
+      <ul className="revenueTime__list">
+        <li className="revenueTime__item">
+          <input type="radio" name="revenueTime" id="today" />
+          <label htmlFor="today">Hoje</label>
+        </li>
+        <li className="revenueTime__item">
+          <input type="radio" name="revenueTime" id="7-days" />
+          <label htmlFor="7-days">7 dias</label>
+        </li>
+        <li className="revenueTime__item">
+          <input type="radio" name="revenueTime" id="15-days" />
+          <label htmlFor="15-days">15 dias</label>
+        </li>
+        <li className="revenueTime__item">
+          <input type="radio" name="revenueTime" id="1-month" />
+          <label htmlFor="1-month">1 mês</label>
+        </li>
+        <li className="revenueTime__item">
+          <input type="radio" name="revenueTime" id="1-year" />
+          <label htmlFor="6-month">6 mês</label>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- src/pages/dashboard/components/CompanyDashboard/index.tsx
+++ src/pages/dashboard/components/CompanyDashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DashboardType } from './DashboardType';
+import { RevenueTime } from './RevenueTime';
 
 // Styles
 import './styles.scss';
@@ -8,6 +9,12 @@ export const CompanyDashboard = () => {
   return (
     <section className='dashboard'>
       <DashboardType />
+      <div className="revenue">
+        <h2>
+          Receita
+        </h2>
+        <RevenueTime />
+      </div>
     </section>
   );
 };
