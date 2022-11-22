e6c70e09e38742f742eec34959b848fd4a4ad969
Nalberth <nalberthsantosr@gmail.com>
2022-11-22 15:28:24 -0300

    fix: add arrow icon if item has children

--- src/components/Aside/NavItem/index.tsx
+++ src/components/Aside/NavItem/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 type NavItemProps = {
-  name: string
-  icon: JSX.Element
-  hasChildren: boolean
-}
+  name: string;
+  icon: JSX.Element;
+  hasChildren: boolean;
+};
 
 export const NavItem = ({ name, icon, hasChildren }: NavItemProps) => {
   return (
     <li className="navbar__item">
       <div className="iconWrapper">{icon}</div>
       <p className="itemName">{name}</p>
+      {hasChildren && (
+        <div className="arrowIcon">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.25024 8.12451L10.0002 11.8745L13.7502 8.12451" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </div>
+      )}
     </li>
-  )
-}
+  );
+};
